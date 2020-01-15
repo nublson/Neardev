092124a1ed5649db8e84a53d016b3a50636dbdc6
nubelsondev <nubelsondev@hotmail.com>
2020-01-15 12:55:38 +0000

    fix(component): create structure and style for main component

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -3,7 +3,100 @@ import React from 'react'
 import './style.scss'
 
 const Main = () => {
-	return <main></main>
+	return (
+		<main>
+			<ul>
+				<li className='dev-item'>
+					<header>
+						<img
+							src='https://avatars1.githubusercontent.com/u/46088089?v=4'
+							alt='Nubelson Fernandes'
+						/>
+						<div className='user-info'>
+							<strong>Nubelson Fernandes</strong>
+							<span>NodeJS, ReactJS and React Native</span>
+						</div>
+					</header>
+					<p>
+						A 22 years old self-taugth Full-Stack Developer with
+						Node and React from Angola, currently living in
+						Portugal.
+					</p>
+					<a
+						href='https://github.com/nubelsondev'
+						target='_blank'
+						rel='noopener noreferrer'
+					>
+						Access Github Profile
+					</a>
+				</li>
+				<li className='dev-item'>
+					<header>
+						<img
+							src='https://avatars1.githubusercontent.com/u/46088089?v=4'
+							alt='Nubelson Fernandes'
+						/>
+						<div className='user-info'>
+							<strong>Nubelson Fernandes</strong>
+							<span>NodeJS, ReactJS and React Native</span>
+						</div>
+					</header>
+					<p>
+						A 22 years old self-taugth Full-Stack Developer with
+						Node and React from Angola, currently living in
+						Portugal.
+					</p>
+					<a
+						href='https://github.com/nubelsondev'
+						target='_blank'
+						rel='noopener noreferrer'
+					>
+						Access Github Profile
+					</a>
+				</li>
+				<li className='dev-item'>
+					<header>
+						<img
+							src='https://avatars1.githubusercontent.com/u/46088089?v=4'
+							alt='Nubelson Fernandes'
+						/>
+						<div className='user-info'>
+							<strong>Nubelson Fernandes</strong>
+							<span>NodeJS, ReactJS and React Native</span>
+						</div>
+					</header>
+					<p>
+						A 22 years old self-taugth Full-Stack Developer with
+						Node and React from Angola, currently living in
+						Portugal.
+					</p>
+					<a href='https://github.com/nubelsondev'>
+						Access Github Profile
+					</a>
+				</li>
+				<li className='dev-item'>
+					<header>
+						<img
+							src='https://avatars1.githubusercontent.com/u/46088089?v=4'
+							alt='Nubelson Fernandes'
+						/>
+						<div className='user-info'>
+							<strong>Nubelson Fernandes</strong>
+							<span>NodeJS, ReactJS and React Native</span>
+						</div>
+					</header>
+					<p>
+						A 22 years old self-taugth Full-Stack Developer with
+						Node and React from Angola, currently living in
+						Portugal.
+					</p>
+					<a href='https://github.com/nubelsondev'>
+						Access Github Profile
+					</a>
+				</li>
+			</ul>
+		</main>
+	)
 }
 
 export default Main
