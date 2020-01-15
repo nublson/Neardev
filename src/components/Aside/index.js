90b0e8e1bb1b565ad3d533872c12fcf4cdb06c99
nubelsondev <nubelsondev@hotmail.com>
2020-01-15 13:39:16 +0000

    feat(functionality): get current position

--- src/components/Aside/index.js
+++ src/components/Aside/index.js
@@ -1,19 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Formik, Form, Field } from 'formik'
 
 import './style.scss'
 
 const Aside = () => {
+	const [latitude, setLatitude] = useState('')
+	const [longitude, setLongitude] = useState('')
+
+	useEffect(() => {
+		navigator.geolocation.getCurrentPosition(
+			position => {
+				const { latitude, longitude } = position.coords
+
+				setLatitude(latitude)
+				setLongitude(longitude)
+			},
+			err => {
+				console.log(err)
+			},
+			{
+				timeout: 30000
+			}
+		)
+	}, [])
+
 	return (
 		<Formik
 			initialValues={{
 				github_username: '',
 				techs: '',
-				latitude: '',
-				longitude: ''
+				latitude: latitude,
+				longitude: longitude
 			}}
-			onSubmit={values => {
+			onSubmit={(values, actions) => {
 				console.log(values)
+
+				actions.resetForm()
 			}}
 		>
 			{props => (
@@ -38,12 +60,22 @@ const Aside = () => {
 						<div className='input-group'>
 							<div className='input-block'>
 								<label htmlFor='latitude'>Latitude</label>
-								<Field name='latitude' id='latitude' />
+								<Field
+									type='number'
+									name='latitude'
+									id='latitude'
+									value={(props.values.latitude = latitude)}
+								/>
 							</div>
 
 							<div className='input-block'>
 								<label htmlFor='longitude'>Longitude</label>
-								<Field name='longitude' id='longitude' />
+								<Field
+									type='number'
+									name='longitude'
+									id='longitude'
+									value={(props.values.longitude = longitude)}
+								/>
 							</div>
 						</div>
 
